a8409bc7bbfddde8467ecdc1a2c5948b99609aa9
Eric <8759230+C1erlc@users.noreply.github.com>
2024-03-27 23:59:52 -0400

    started admin dashboard

--- src/main/java/org/group68/AdminDashboard.java
+++ src/main/java/org/group68/AdminDashboard.java
@@ -6,9 +6,52 @@ import java.sql.Connection;
 public class AdminDashboard extends JFrame {
 
     private Connection databaseConnection;
+    private JPanel mainPane;
 
-    public AdminDashboard(Connection databaseConnection)
+    //TODO: PUT THE DATABASE CONNECTION BACK IN CONSTRUCTOR PARAMS LATER
+    public AdminDashboard()
     {
         this.databaseConnection = databaseConnection;
+        setTitle("Admin Dashboard");
+        mainPane = new JPanel();
+        setContentPane(mainPane);
+        JMenuBar menuBar = new JMenuBar();
+        JMenu room = new JMenu("Room Booking");
+        room.addActionListener(e -> showRoom());
+        menuBar.add(room);
+        JMenu equipment = new JMenu("Equipment Maintenance");
+        equipment.addActionListener(e -> showEquipment());
+        menuBar.add(equipment);
+        JMenu classes = new JMenu("Class Schedule");
+        classes.addActionListener(e -> showClasses());
+        menuBar.add(classes);
+        JMenu billing = new JMenu("Billing");
+        billing.addActionListener(e -> showBilling());
+        menuBar.add(billing);
+        this.setJMenuBar(menuBar);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setSize(800, 600);
+        setLocationRelativeTo(null);
+        setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        AdminDashboard dashboard = new AdminDashboard();
+    }
+
+    private void showRoom(){
+        System.out.println("room");
+    }
+
+    private void showEquipment(){
+        System.out.println("equipment");
+    }
+
+    private void showClasses(){
+        System.out.println("classes");
+    }
+
+    private void showBilling(){
+        System.out.println("billing");
     }
 }
