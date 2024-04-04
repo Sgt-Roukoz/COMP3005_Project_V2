47013872a4d8482d7e4f3b2bec6b9e7641012480
Nirjhor <masrur.husain4@gmail.com>
2024-04-04 09:45:41 -0400

    Began work on registering Jpanel for new members

--- src/main/java/org/group68/LoginPage.java
+++ src/main/java/org/group68/LoginPage.java
@@ -1,8 +1,11 @@
 package org.group68;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.WindowEvent;
 import java.sql.*;
+import java.util.Date;
 
 public class LoginPage extends JFrame{
     private JPanel Login;
@@ -14,10 +17,50 @@ public class LoginPage extends JFrame{
     private JComboBox memberType;
     private JLabel iAm;
     private JLabel error;
+    private JTextField firstname;
+    private JTextField lastname;
+    private JPanel register;
+    private JTextField phone;
+    private JTextField cardNumber;
+    private JPasswordField pin;
+    private JButton registerButton;
+    private JLabel pinText;
+    private JLabel cardnumText;
+    private JLabel emailText;
+    private JLabel phoneText;
+    private JLabel lastnameText;
+    private JLabel firstnameText;
+    private JFormattedTextField restingHR;
+    private JFormattedTextField bloodPressure;
+    private JFormattedTextField weight;
+    private JLabel hrText;
+    private JLabel bpText;
+    private JLabel weightText;
+    private JFormattedTextField email;
+    private JLabel invalid;
     private Connection connection;
     public LoginPage(Connection connection){
         this.connection = connection;
         error.setVisible(false);
+        invalid.setVisible(false);
+
+        loginButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                try{
+                    checkLoginDetails();
+                }catch (SQLException x){
+                    x.printStackTrace();
+                }
+            }
+        });
+
+        registerButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
     }
 
     public void checkLoginDetails() throws SQLException {
@@ -106,6 +149,37 @@ public class LoginPage extends JFrame{
             }
         }else{
             error.setVisible(true);
+        }
+    }
+
+    public void enterRegisterDetails() throws SQLException {
+        if(firstname.getText() != "" && lastname.getText() != "" && email.getText() != "@" && phone.getText() != "" && cardNumber.getText() != "" && pin.getText() != "" && restingHR.getText() != "" && weight.getText() != "" && bloodPressure.getText() != ""){
+            String fname = firstname.getText();
+            String lname = lastname.getText();
+            String emailInfo = email.getText();
+            String phoneNum = phone.getText();
+            String cardNum = cardNumber.getText();
+            String pinny = pin.getText();
+            String heartRate = restingHR.getText();
+            String weighty = weight.getText();
+            String pressure = bloodPressure.getText();
+            Date today = new Date();
+            today.setHours(0);
+
+            Statement stmt = connection.createStatement();
+            ResultSet rs;
+            String SQL;
+
+            try {
+                SQL = "INSERT INTO GymMembers (email, member_password FROM MemberLogins WHERE member_username =";
+                rs = stmt.executeQuery(SQL); // Process the result
+            }catch (Exception e){
+                error.setVisible(true);
+            }
+
+
+        }else{
+            invalid.setVisible(true);
         }
     }
 
