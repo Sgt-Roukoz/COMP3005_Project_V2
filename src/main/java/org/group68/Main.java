6248f9075dccecec9c4be92382b857acd0e74b7b
Marwan Zeid <45441599+Sgt-Roukoz@users.noreply.github.com>
2024-04-13 14:28:14 -0400

    quick loginpage change to display properly

--- src/main/java/org/group68/Main.java
+++ src/main/java/org/group68/Main.java
@@ -1,7 +1,24 @@
 package org.group68;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Connection databaseConnection = null;
+        try {
+            Class.forName("org.postgresql.Driver");
+            String url = "jdbc:postgresql://localhost:5432/FitnessClub";
+            String user = "postgres";
+            String password = "z3i0";
+            databaseConnection = DriverManager.getConnection(url, user, password);
+            if (databaseConnection != null) System.out.println("Connected Successfully");
+            else System.out.println("Connection Failed");
+        } catch (SQLException | ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        new LoginPage(databaseConnection);
     }
 }
--- src/main/java/org/group68/MemberDashboard.java
+++ src/main/java/org/group68/MemberDashboard.java
@@ -1241,20 +1241,6 @@ public class MemberDashboard extends JFrame {
 
     public static void main(String[] args) {
 
-        Connection databaseConnection = null;
-        try {
-            Class.forName("org.postgresql.Driver");
-            String url = "jdbc:postgresql://localhost:5432/FitnessClub";
-            String user = "postgres";
-            String password = "z3i0";
-            databaseConnection = DriverManager.getConnection(url, user, password);
-            if (databaseConnection != null) System.out.println("Connected Successfully");
-            else System.out.println("Connection Failed");
-        } catch (SQLException | ClassNotFoundException e) {
-            e.printStackTrace();
-        }
-
-        new MemberDashboard(databaseConnection, 1);
     }
 
 }
